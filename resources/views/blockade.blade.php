fa2076d50c81d06f4c01c2ad4c5f7aa337fafe0d
Joco <joco@klever.co.uk>
2024-03-16 22:18:03 +0000

    simple single landing page

---
+++ resources/views/blockade.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>DominoNet</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+    <link rel="stylesheet" href="/assets/css/main.css"/>
+    <noscript>
+        <link rel="stylesheet" href="/assets/css/noscript.css"/>
+    </noscript>
+</head>
+{{--<x-body>--}}
+<body>
+<div class="align-center" style="position: absolute; top: 50%; left: 45%">
+    Under Construction
+</div>
+</body>
+{{--</x-body>--}}
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+//    return view('index');
+    return view('blockade');
 });
 
 Route::get('/generic', function () {
